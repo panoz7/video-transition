824cdd6ebb73892052e49e40c6c5610019379784
Griffin Friedman <gfriedm@Griffins-MBP.fios-router.home>
2019-12-04 18:57:21 -0500

    Export videos in format ipad supports

--- data.js
+++ data.js
@@ -1,12 +1,12 @@
 export let data = {
     'video1' : {
-        video: 'videos/colorshift1.mp4',
+        video: './videos/colorshift3.mp4',
         pauseFrame: 150,
         pauseDuration: 10,
         frameRate: 30
     }, 
     'video2' : {
-        video: 'videos/colorshift2.mp4',
+        video: './videos/colorshift4.mp4',
         pauseFrame: 150,
         pauseDuration: 10,
         frameRate: 30
