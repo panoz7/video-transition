57c908c9f8b4f10db646ef052334ca388188f009
Griffin Friedman <gfriedm@Griffins-MBP.fios-router.home>
2019-12-04 18:20:41 -0500

    Add framerate to metadata

--- data.js
+++ data.js
@@ -2,11 +2,13 @@ export let data = {
     'video1' : {
         video: 'videos/colorshift1.mp4',
         pauseFrame: 150,
-        pauseDuration: 10
+        pauseDuration: 10,
+        frameRate: 30
     }, 
     'video2' : {
         video: 'videos/colorshift2.mp4',
         pauseFrame: 150,
-        pauseDuration: 10
+        pauseDuration: 10,
+        frameRate: 30
     }
 }
