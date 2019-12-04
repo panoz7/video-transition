6742ae66dcc2cb84e35dec2b1889e7f0efdaf889
Griffin Friedman <gfriedm@Griffins-MBP.fios-router.home>
2019-12-04 17:54:05 -0500

    Fix video file path

--- data.js
+++ data.js
@@ -1,11 +1,11 @@
 export let data = {
     'video1' : {
-        video: 'videos/colorshift1.mp5',
+        video: 'videos/colorshift1.mp4',
         pauseFrame: 150,
         pauseDuration: 10
     }, 
     'video2' : {
-        video: 'videos/colorshift1.mp5',
+        video: 'videos/colorshift1.mp4',
         pauseFrame: 150,
         pauseDuration: 10
     }
