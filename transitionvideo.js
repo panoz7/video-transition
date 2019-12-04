8ce761b4f6db4b97e813f96915b9a7c7991ca695
Griffin Friedman <gfriedm@Griffins-MBP.fios-router.home>
2019-12-04 18:34:15 -0500

    Make video pause at pause frame

--- transitionvideo.js
+++ transitionvideo.js
@@ -4,15 +4,34 @@ export class TransitionVideo {
         this.metadata = metadata;
     }
 
-    playVideo(container, fromVideo, moveFromTo) {
+    async playVideo(container, fromVideo, moveFromTo) {
         this.container = container; 
 
         if (fromVideo) {
+            // Move the from video into the other container
             fromVideo.moveTo(moveFromTo);
         }
 
         this.createVideo();
+        await this.playVideoUntilPause();
+    }
+
+    async playVideoUntilPause() {
         this.video.play();
+
+        // Pause the video at the pause frame
+        await this.trackUntilFrame(this.metadata.pauseFrame);
+        this.video.pause();
+
+        // Jump to the first pause point
+        this.jumpToFirstPause();
+
+        return true; 
+    }
+
+    jumpToFirstPause() {
+        const pauseTime = this.getTimeFromFrame(this.metadata.pauseFrame + this.metadata.pauseDuration);
+        this.video.currentTime = pauseTime; 
     }
 
     moveTo(container) {
@@ -31,6 +50,33 @@ export class TransitionVideo {
 
         // Add it to the container
         this.container.appendChild(this.video);
+    }
+
+    getCurrentFrame() {
+        return Math.floor(this.video.currentTime * this.metadata.frameRate);
+    }
+
+    getFrameFromTime(time) {
+        return Math.floor(time * this.metadata.frameRate);
+    }
+
+    getTimeFromFrame(frame) {
+        return frame / this.metadata.frameRate;
+    }
+
+    trackUntilFrame(frame) {
+        return new Promise((resolve) => {
+            
+            let checkTime = () => {
+                const currentFrame = this.getCurrentFrame();
+                if (currentFrame >= frame) {
+                    resolve()
+                }
+                window.requestAnimationFrame(checkTime);
+            }
+
+            window.requestAnimationFrame(checkTime);
+        })
     }
 
 }
