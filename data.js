26c480ba1f93b059989a8cbb0fc690bb66de572c
Griffin Friedman <gfriedm@Griffins-MBP.fios-router.home>
2019-12-04 19:16:41 -0500

    Rerender videos

--- data.js
+++ data.js
@@ -1,13 +1,13 @@
 export let data = {
     'video1' : {
-        video: './videos/colorshift3.mp4',
-        pauseFrame: 150,
+        video: './videos/sequence1.mp4',
+        pauseFrame: 60,
         pauseDuration: 10,
         frameRate: 30
     }, 
     'video2' : {
-        video: './videos/colorshift4.mp4',
-        pauseFrame: 150,
+        video: './videos/sequence2.mp4',
+        pauseFrame: 60,
         pauseDuration: 10,
         frameRate: 30
     }
